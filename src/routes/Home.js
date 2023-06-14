91137e72b5763ae25afb34534a23d351f1524653
Ankit Verma <92419249+ankiit369@users.noreply.github.com>
2023-06-14 07:09:06 +0000

    Updated Hero section for all pages.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,19 @@
 import Navbar from "../components/Navbar";
-
+import Hero from "../components/Hero";
+import decor1_bkl from "../assets/decor1_bkl.jpg"
 function Home(){
     return(
         <>
         <Navbar />
-        <h1>This is Home</h1>
+        <Hero 
+        cName="hero"
+        heroImg={decor1_bkl}
+        title="Banarasi Kulhad Chai"
+        text="Taste That Matters The Most"
+        buttonText="Take A Sip"
+        url="/"
+        btnClass="show"
+        />
         </>
     )
 }
